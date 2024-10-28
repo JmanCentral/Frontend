87adb939b0db38ef25c972b8f81a7129609d7edf
jmancipet <jmancipet@ucentral.edu.co>
2024-10-28 13:20:07 -0500

    Clase pregunta

--- app/src/main/java/Utils/APIS.java
+++ app/src/main/java/Utils/APIS.java
@@ -3,8 +3,20 @@ package Utils;
 public class APIS {
 
     public static final String URL_001 = "http://192.168.10.10:8862/Trivia/usuarios/";
+    public static final String URL_002 = "http://192.168.10.10:8862/Trivia/preguntas/";
 
     public static UsuarioService getUsuarioService(){
         return Cliente.getCliente(URL_001).create(UsuarioService.class);
     }
+
+    public static PreguntaService getPreguntaService(){
+        return Cliente.getCliente(URL_002).create(PreguntaService.class);
+    }
+
+    /*
+    public static HistorialService getRespuestaService(){
+        return Cliente.getCliente(URL_001).create(HistorialService.class);
+    }
+
+     */
 }
---
+++ app/src/main/java/com/arquitectura/apirest/Entidades/Pregunta.java
@@ -0,0 +1,99 @@
+package com.arquitectura.apirest.Entidades;
+
+public class Pregunta {
+
+    private Long id_pregunta;
+    private String pregunta;
+    private String op1;
+    private String op2;
+    private String op3;
+    private String op4;
+    private String respuesta;
+    private String categoria;
+    private String dificultad;
+
+    public Pregunta(Long id_pregunta, String pregunta, String op1, String op2, String op3, String op4, String respuesta, String categoria, String dificultad) {
+        this.id_pregunta = id_pregunta;
+        this.pregunta = pregunta;
+        this.op1 = op1;
+        this.op2 = op2;
+        this.op3 = op3;
+        this.op4 = op4;
+        this.respuesta = respuesta;
+        this.categoria = categoria;
+        this.dificultad = dificultad;
+
+    }
+
+    public Long getId_pregunta() {
+        return id_pregunta;
+    }
+
+    public void setId_pregunta(Long id_pregunta) {
+        this.id_pregunta = id_pregunta;
+    }
+
+    public String getPregunta() {
+        return pregunta;
+    }
+
+    public void setPregunta(String pregunta) {
+        this.pregunta = pregunta;
+    }
+
+    public String getOp1() {
+        return op1;
+    }
+
+    public void setOp1(String op1) {
+        this.op1 = op1;
+    }
+
+    public String getOp2() {
+        return op2;
+    }
+
+    public void setOp2(String op2) {
+        this.op2 = op2;
+    }
+
+    public String getOp3() {
+        return op3;
+    }
+
+    public void setOp3(String op3) {
+        this.op3 = op3;
+    }
+
+    public String getOp4() {
+        return op4;
+    }
+
+    public void setOp4(String op4) {
+        this.op4 = op4;
+    }
+
+    public String getRespuesta() {
+        return respuesta;
+    }
+
+    public void setRespuesta(String respuesta) {
+        this.respuesta = respuesta;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getDificultad() {
+        return dificultad;
+    }
+
+    public void setDificultad(String dificultad) {
+        this.dificultad = dificultad;
+    }
+}
