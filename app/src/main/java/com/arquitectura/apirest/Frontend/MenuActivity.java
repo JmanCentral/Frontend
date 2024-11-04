34119f0c6618a8fa10e67913f92f237cee10ad40
jmancipet <jmancipet@ucentral.edu.co>
2024-11-03 21:45:18 -0500

    Perfil

--- app/src/main/java/com/arquitectura/apirest/Frontend/MenuActivity.java
+++ app/src/main/java/com/arquitectura/apirest/Frontend/MenuActivity.java
@@ -4,16 +4,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.arquitectura.apirest.R;
 
@@ -98,7 +94,7 @@ public class MenuActivity extends AppCompatActivity {
     public void editarContrasena(View view) {
 
 
-        Intent intent = new Intent(this, EditarContraActivity.class);
+        Intent intent = new Intent(this, Perfillogros.class);
         startActivity(intent);
     }
     public void salir(View view) {
--- app/src/main/java/com/arquitectura/apirest/Frontend/Perfillogros.java
+++ app/src/main/java/com/arquitectura/apirest/Frontend/Perfillogros.java
@@ -10,13 +10,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.arquitectura.apirest.R;
 
-public class EditarContraActivity extends AppCompatActivity {
+public class Perfillogros extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_editar_contra);
+        setContentView(R.layout.activity_perfillogros);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
